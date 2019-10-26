3379dbfb5c440872184fde640be8156bd40c9ce2
boleks <btrosic@gmail.com>
2019-10-26 10:36:18 +0200

    initial project

---
+++ src/main/java/com/boleks/spg5recipeapp/Spg5RecipeAppApplication.java
@@ -0,0 +1,13 @@
+package com.boleks.spg5recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spg5RecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spg5RecipeAppApplication.class, args);
+    }
+
+}
