37f4bd94477cba72ae8b8390f8d7e2e34562fe53
boleks <btrosic@gmail.com>
2019-11-06 19:15:00 +0100

    add recipe page add recipe test

--- src/main/java/com/boleks/spg5recipeapp/bootstrap/RecipeBootstrap.java
+++ src/main/java/com/boleks/spg5recipeapp/bootstrap/RecipeBootstrap.java
@@ -144,6 +144,9 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
         guacRecipe.getCategories().add(americanCategory);
         guacRecipe.getCategories().add(mexicanCategory);
 
+        guacRecipe.setUrl("https://www.simplyrecipes.com/recipes/perfect_guacamole/");
+        guacRecipe.setSource("Simply Recipes");
+
         //add to return list
         recipes.add(guacRecipe);
 
